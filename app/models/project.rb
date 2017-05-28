1cc31e7a8b7964fc466c5f8b3062cf659f1a0cd9
Oleg Savickij <katexizis94@gmail.com>
2017-05-29 00:58:52 +0300

    Add project simple CRUD

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
   has_many :tasks
+  validates :name, presence: true,
+            length: { minimum: 5 }
+  validates :summary, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
